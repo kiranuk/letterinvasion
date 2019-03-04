22eae100e7ea301085ed849d62fb54e485c69346
kiranuk <Kiran6475@icloud.com>
2019-03-04 12:56:05 +0530

    implement test for draw_invader and passed

--- test_invader.py
+++ test_invader.py
@@ -9,6 +9,10 @@ def test_create_random_letter():
 
 def test_move_invaders():
     height = 500
-    assert (letter_invader.move_invaders({(0, 300):'r'}, window) == {(1, 300) : 'r'}for _ in range(0, height))
-    assert (letter_invader.move_invaders({(501, 300):'r'}, window) == {(500, 300) : 'r'}for _ in range(0, height))
+    assert (letter_invader.move_invaders({(0, 300):'r'}, window) == {(1, 300) : 'r'} for _ in range(0, height))
+    assert (letter_invader.move_invaders({(501, 300):'r'}, window) == {(500, 300) : 'r'} for _ in range(0, height))
 
+def test_draw_invaders():
+    hieght = 500
+    width = 500
+    assert (letter_invader.draw_invaders({(3, 400) : 'd'}, window) == {(3, 400) : 'd'} for _ in range(0,500))
