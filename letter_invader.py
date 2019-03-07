2b0a972e645436ad684723ba5ff58454d4b9f9d8
kiranuk <Kiran6475@icloud.com>
2019-03-07 12:25:32 +0530

    kill_invader function

--- letter_invader.py
+++ letter_invader.py
@@ -6,52 +6,47 @@ import getch
 
 
 def max_dimensions(window):
-    height, width =   window.getmaxyx()
-    return height - 1, width
+    height, width = window.getmaxyx()
+    return height - 2, width - 1
 def create_random_letter(window):
     width = max_dimensions(window)[1]
     letter = random.choice(string.ascii_lowercase)
     column = random.randrange(0, width)
     return 0, column, letter
-def move_invaders(prev, window):
+def move_invaders(prev):
     new = {}
     for (row, column), char in prev.items():
-        height, _ = max_dimensions(window)
         new_row = row + 1
-        if new_row > height:
-            new_row -= 1
         new[(new_row, column)] = char
     return new
 
 def draw_invaders(invaders, window):
     for (row, column), char in invaders.items():
         height, width = max_dimensions(window)
+        if row > height or column > width:
+            continue
+        window.addch(row, column, char)
+def kill_invader(invaders, window):
+    q = window.getch()
+    if q == -1:
+        if q == 
+            char = ' '
     window.addch(row, column, char)
-def kill_invader(letter, user_input):
-    k = {}
-    for char in invaders.values():
-        user_input = getch.getch()
-        if  char == user_input:
-            k.replace(char, ' ')
-    return k
 
-#def life(invaders, window):
- #   lifeterm = 10
-  #  if row > height or column > width:
-   #     lifeterm -= 1
-
-    #return lifeterm 
-
-
+            
 def main(window):
     curses.curs_set(0)
-    invaders = {}
+    invaders = {}    
+    inkey = window.getch()
+    
     while True:
         window.clear()
-        invaders = move_invaders(invaders, window)
+        window.nodelay(True)
+        invaders = move_invaders(invaders)
         invader = create_random_letter(window)
         invaders[(invader[0], invader[1])] = invader[2]
         draw_invaders(invaders, window)
+        inkey(invaders, window)
         time.sleep(1.5)
         window.refresh()
 
