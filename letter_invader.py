e2333d2a7de5702cf1a55445853abdf695fd88d4
kiranuk <Kiran6475@icloud.com>
2019-03-08 16:36:24 +0530

    window args changed to height and width and apply global variables. in test count_life gives height value

--- letter_invader.py
+++ letter_invader.py
@@ -3,7 +3,6 @@ import string
 import random
 import time
 
-
 def max_dimensions(window):
     height, width = window.getmaxyx()
     return height - 2, width - 1
@@ -13,10 +12,9 @@ def create_random_letter(width):
     column = random.randrange(0, width)
     return 0, column, letter
 
-def move_invaders(invaders, window):
+def move_invaders(invaders, height):
     new = {}
-    for (row, column), char in invaders.items():
-        height, _ = max_dimensions(window)
+    for (row, column), char in invaders.items():  
         new_row = row + 1
         if new_row > height:
             new_row -= 1 
@@ -25,7 +23,6 @@ def move_invaders(invaders, window):
 
 def draw_invaders(invaders, window):
     for (row, column), char in invaders.items():
-        height, width = max_dimensions(window)
         if row > height or column > width:
             continue
         window.addch(row, column, char)
@@ -37,8 +34,8 @@ def kill_invader(invaders, q):
 def count_life(invaders, height):
     life = 10
     max_row = height - 1
-    for (row, column) in invaders.keys():
-        if (max_row, column):
+    for (row, column), char in invaders.items():
+        if max_row == row:
             life -= 1
     return life
 
@@ -46,22 +43,24 @@ def count_life(invaders, height):
 def main(window):
     curses.curs_set(0)
     invaders = {}
-    run = True
-    while run:
+    global height, width
+    height, width = max_dimensions(window)
+    while True:
         window.clear()
         window.nodelay(True)
-        height, width = max_dimensions(window)
         invader = create_random_letter(width)
-        invaders = move_invaders(invaders, window)
+        invaders = move_invaders(invaders, height)
         invaders[(invader[0], invader[1])] = invader[2]
         q = window.getch()
+        if q != -1:
+            invaders = kill_invader(invaders, chr(q))
         draw_invaders(invaders, window)
         window.refresh()
         kill_invader(invaders, q)
         time.sleep(0.4)
         window.refresh()
-    if count_life(invaders, height) == 0:
-        run = False
+        if count_life(invaders, height) == 0:
+            break
 
 if __name__ == '__main__':
     curses.wrapper(main)
--- test_invader.py
+++ test_invader.py
@@ -2,31 +2,25 @@ import letter_invader
 import random
 import string
 
+global height, width 
+height, width = 500, 500
 def test_create_random_letter():
-    width = 400
     letter = random.choice(string.ascii_lowercase)
-    column = random.randrange(0,400)
+    column = random.randrange(0, 500)
     assert (letter_invader.create_random_letter(width)  == 0, column, letter) 
     assert (letter_invader.create_random_letter(width)  == 0, column, letter)
 
 def test_move_invaders():
-    height = 500
-    assert (letter_invader.move_invaders({(0, 300):'r'}, window) == {(1, 300) : 'r'} for _ in range(0, 500))
-    assert (letter_invader.move_invaders({(400, 300):'r'}, window) == {(401, 300) : 'r'} for _ in range(0, 500))
-
-def test_draw_invaders():
-    height = 500
-    width = 500
-    assert (letter_invader.draw_invaders({(3, 400) : 'd'}, window) == {(3, 400) : 'd'} for _ in range(0, 500))
+    assert letter_invader.move_invaders({(0, 300):'r'}, height) == {(1, 300) : 'r'}
+    assert letter_invader.move_invaders({(400, 300):'r'}, height) == {(401, 300) : 'r'}
 
 def test_kill_invader():
     value = {(10, 50): 'd', (20, 30): 'k', (100, 230): 'r', (25,25): 'j'}
     result = {(10, 50): 'd', (25, 25): 'j', (100, 230): 'r'}
-    assert (letter_invader.kill_invader(value, 'k') == result)
-    assert (letter_invader.kill_invader(value, 'a') == value) 
+    assert letter_invader.kill_invader(value, 'k') == result
+    assert letter_invader.kill_invader(value, 'a') == value
 
 def test_count_life():
-    height = 500
-    value = {(1, 3): 'a', (2, 6): 's', (500,200): 'd'}
-    result = 7
-    assert (letter_invader.count_life(value, height) == result)
+    value = {(500,200): 'd'}
+    result = 9
+    assert letter_invader.count_life(value, 501) == result
