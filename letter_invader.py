4b8930b5e020d02f957c53b76b10024803c57c76
kiranuk <Kiran6475@icloud.com>
2019-03-16 16:04:13 +0530

    Trying to del lowest letter from screen for that kill_invader function has to give for loop but it fails

--- letter_invader.py
+++ letter_invader.py
@@ -21,25 +21,27 @@ def move_invaders(invaders, height):
         new[(new_row, column)] = char
     return new
 
-def draw_invaders(invaders, window):
+def draw_invaders(invaders, invader, window):
+    invaders[(invader[0], invader[1])] = invader[2]
     for (row, column), char in invaders.items():
         if row > height or column > width:
             continue
         window.addch(row, column, char)
 
 def kill_invader(invaders, q):
-    invaders = {key: value for key, value in invaders.items() if value is not q}
+    for (row, column) in invaders.keys():
+        max_row = row - 1
+        if row == max_row:
+            invaders = {key: value for key, value in invaders.items() if value is not q if key is not max_row}
     return invaders
 
 def count_life(invaders, height):
-    life = 10
+    count = 0
     for (row, column), char in invaders.items(): 
         max_row = height - 1
-        if row == max_row:
-            life -= 1
-        if life == 0:
-            break
-    return life
+        if row > max_row:
+            count += 1
+    return count
 
 
 
@@ -50,19 +52,20 @@ def main(window):
     height, width = max_dimensions(window)
     t = 0.3
     window.nodelay(True)
+    t += 0.1
     while True:
         window.clear()
         invader = create_random_letter(width)
         invaders = move_invaders(invaders, height)
-        invaders[(invader[0], invader[1])] = invader[2]
         q = window.getch()
-        if q != -1:
-            invaders = kill_invader(invaders, chr(q))
-        draw_invaders(invaders, window)
+        draw_invaders(invaders, invader, window)
         window.refresh()
         kill_invader(invaders, q)
-        time.sleep(t+0.1)     
-        count_life(invaders, height)
+        time.sleep(t)
+        if q != -1:
+            invaders = kill_invader(invaders,chr(q))
+        if count_life(invaders, height) == 10:
+            break
         window.refresh()
 if __name__ == '__main__':
     curses.wrapper(main)
--- test_invader.py
+++ test_invader.py
@@ -9,12 +9,12 @@ def test_move_invaders():
     assert letter_invader.move_invaders({(400, 300):'r'}, height) == {(401, 300) : 'r'}
 
 def test_kill_invader():
-    value = {(10, 50): 'd', (20, 30): 'k', (100, 230): 'r', (25,25): 'j'}
-    result = {(10, 50): 'd', (25, 25): 'j', (100, 230): 'r'}
+    value = {(10, 50): 'd', (20, 30): 'k', (30,50): 'k', (100, 230): 'r', (25,25): 'j'}
+    result = {(10, 50): 'd', (20, 30): 'k', (25, 25): 'j', (100, 230): 'r'}
     assert letter_invader.kill_invader(value, 'k') == result
     assert letter_invader.kill_invader(value, 'a') == value
 
 def test_count_life():
-    value = {(500,200): 'd', (200,200): 'e', (500,300): 'l',(300,200):'o'}
-    result = 8
+    value = {(501,200): 'd', (200,200): 'e', (501,300): 'l',(300,200):'o'}
+    result = 2
     assert letter_invader.count_life(value, 501) == result
