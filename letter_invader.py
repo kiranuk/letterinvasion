94e9057b68f010ce60fb505e12bacb80d9c1075b
kiranuk <Kiran6475@icloud.com>
2019-03-24 20:15:05 +0530

    CHanges made to give color for that changes made in main and removes window.refresh in bottom lines, and also remove white spaces and give two lines after each functions

--- letter_invader.py
+++ letter_invader.py
@@ -3,39 +3,40 @@ import string
 import random
 import time
 
+
 def max_dimensions(window):
     height, width = window.getmaxyx()
     return height - 2, width - 1
+
 
 def create_random_letter(width):
     letter = random.choice(string.ascii_lowercase)
     column = random.randrange(0, width)
     return 0, column, letter
 
-#def for_color_letter(window, letter):
 
 def move_invaders(invaders, height):
     new = {}
     for (row, column), char in invaders.items():
         new_row = row + 1
         if new_row > height:
-            new_row -= 1 
+            new_row -= 1
         new[(new_row, column)] = char
     return new
 
+
 def draw_invaders(invaders, invader, window):
     invaders[(invader[0], invader[1])] = invader[2]
-    for pos, color in enumerate(colors):
-        curses.init_pair(pos, color, curses.COLOR_BLUE)
     for (row, column), char in invaders.items():
         if row > height or column > width:
             continue
         window.addch(row, column, char)
 
+
 def kill_invaders(invaders, q):
     list1 = []
     for (row, column), char in invaders.items():
-       if char is q:
+        if char is q:
             t = (row, column)
             list1.append(t)
             continue
@@ -43,18 +44,19 @@ def kill_invaders(invaders, q):
         del invaders[max(list1)]
     return invaders
 
+
 def count_life(invaders, height):
     count = 10
-    for (row, column), char in invaders.items(): 
+    for (row, column), char in invaders.items():
         max_row = height - 1
         if row > max_row:
             count -= 1
-    return count 
+    return count
 
 
 def main(window):
-    curses.start_color()
-    curses.init_color(curses.COLOR_)
+    curses.init_color(curses.COLOR_BLACK, 0, 0, 0)
+    curses.init_color(curses.COLOR_WHITE, 1000, 1000, 1000)
     curses.curs_set(0)
     invaders = {}
     global height, width
@@ -72,9 +74,10 @@ def main(window):
         kill_invaders(invaders, q)
         time.sleep(t)
         if q != -1:
-            invaders = kill_invaders(invaders,chr(q))
+            invaders = kill_invaders(invaders, chr(q))
         if count_life(invaders, height) == 0:
             break
-        window.refresh()
+
+
 if __name__ == '__main__':
     curses.wrapper(main)
