97681359e89125f96f560328a1f4ed843ed857f0
kiranuk <Kiran6475@icloud.com>
2019-04-11 02:27:12 +0530

    New test added for eliminating_char() also change count to life in count_life()

--- test_invader.py
+++ test_invader.py
@@ -1,4 +1,4 @@
-import letter_invader
+import game_setup
 import random
 import string
 
@@ -8,18 +8,39 @@ height, width = 500, 500
 
 
 def test_move_invaders():
-    assert letter_invader.move_invaders({(0, 300):'r'}, height) == {(1, 300) : 'r'}
-    assert letter_invader.move_invaders({(400, 300):'r'}, height) == {(401, 300) : 'r'}
+    assert game_setup.move_invaders({(0, 300):('r', 1)}, height) == {(1, 300) : ('r', 1)}
+    assert game_setup.move_invaders({(400, 300):('r', 1)}, height) == {(401, 300) : ('r', 1)}
 
 
 def test_kill_invaders():
-    value = {(10, 50): 'd', (20, 30): 'k', (30,50): 'k', (100, 230): 'r', (25, 25): 'j'}
-    result = {(10, 50): 'd', (20, 30): 'k', (25, 25): 'j', (100, 230): 'r'}
-    assert letter_invader.kill_invaders(value, 'k') == result
-    assert letter_invader.kill_invaders(value, 'a') == value
+    value = {(10, 50): ('d', 0),
+             (20, 30): ('k', 0),
+             (40, 50): ('k', 0),
+             (100, 230): ('r', 0),
+             (25, 25): ('j', 0)}
 
+    result = {(10, 50): ('d', 0),
+             (20, 30): ('k', 0),
+             (40, 50): ('*', 0),
+             (100, 230): ('r', 0),
+             (25, 25): ('j', 0)}
+    assert game_setup.kill_invaders(value, 'k') == result
+    assert game_setup.kill_invaders(value, 'a') == value
+
+def test_eliminating_char():
+    value = {(10, 50): ('d', 0),
+             (20, 30): ('k', 0),
+             (40, 50): ('*', 0),
+             (100, 230): ('r', 0),
+             (25, 25): ('j', 0)}
+
+    result = {(10, 50): ('d', 0),
+             (20, 30): ('k', 0),
+             (100, 230): ('r', 0),
+             (25, 25): ('j', 0)}
+    assert game_setup.eliminating_char(value, (40, 50), 2) == result
 
 def test_count_life():
-    value = {(501, 200): 'd', (200, 200): 'e', (501, 300): 'l', (300, 200): 'o'}
+    value = {(501, 200): ('d', 0), (200, 200): ('e', 0), (501, 300): ('l', 0), (300, 200): ('o', 0)}
     result = 8
-    assert letter_invader.count_life(value, 501) == result
+    assert game_setup.count_life(value, 501) == result
